d9abfceb3b3bcf2d2f752225124dc7be38f6004a
GenericGithUser <blgold567@gmail.com>
2026-04-12 18:22:38 +0800

    feat: Added Syntax Highlighting

--- src/components/CodeRender.jsx
+++ src/components/CodeRender.jsx
@@ -1,11 +1,16 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { gruvboxDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 function CodeRender({ content }){
-    return(
-        <>
-           <pre className="border-4 border-green3-cus rounded-2xl overflow-auto bg-gray-500 text-white w-[77vw] m-auto h-[70vh] max-[500px]:w-[85vw]">
-                {content}
-           </pre>
-        </>
-    )
+    return (
+      <>
+        <div className="code border-4 border-green3-cus rounded-2xl overflow-auto m-auto h-[80vh] w-[160vh] max-[800px]:w-[100vh] max-[500px]:w-[85vw] bg-gray-800">
+          <SyntaxHighlighter style={gruvboxDark} language="java" showLineNumbers="true" lineNumberStyle={{fontSize: '0.8em'}}>
+            {content}
+          </SyntaxHighlighter>
+        </div>
+      </>
+    );
 }
 
 export default CodeRender
